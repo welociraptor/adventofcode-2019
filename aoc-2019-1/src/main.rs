f17ac4dc8b9bf893d5e909f887281ea308c5d0a6
Markku Halinen <markkuh@unity3d.com>
2019-12-01 21:55:00 +0200

    Use iterator + map for part one

--- aoc-2019-1/src/main.rs
+++ aoc-2019-1/src/main.rs
@@ -2,15 +2,14 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("error reading input");
-    let data = input.lines();
-    let mut result_part1: f32 = 0.0;
+
+    let sum: f32 = input.lines().map(|v| fuel(v.parse().unwrap())).sum();
+    println!("Answer for part one: {}", sum); // 3317970
+
     let mut result_part2: f32 = 0.0;
-    for s in data {
-        let mass: f32 = s.parse().unwrap();
+    for s in input.lines() {
+        let f = fuel(s.parse().unwrap());
 
-        let f = fuel(mass);
-
-        result_part1 += f;
         result_part2 += f;
 
         let mut ef = fuel(f);
@@ -19,7 +18,6 @@ fn main() {
             ef = fuel(ef);
         }
     }
-    println!("Answer for part one: {}", result_part1); // 3317970
     println!("Answer for part two: {}", result_part2); // 4974073
 }
 
